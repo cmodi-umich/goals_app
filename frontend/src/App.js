5abd433e9e5eb62bac80c6c6eae88516b5d6b62e
cmodi <cmodi@umich.edu>
2020-03-25 01:10:08 -0400

    Added frontend and basic navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import AppNavbar from './components/AppNavbar';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/AppNavbar.js
@@ -0,0 +1,51 @@
+import React, { Fragment, useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const AppNavbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Goal Setter</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/cmodi-umich/goals_app">GitHub</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>
+            Register       
+          </NavbarText>
+          <NavbarText>
+            .    .
+          </NavbarText>
+          <NavbarText>
+            Login
+          </NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default AppNavbar;
